8a00640f4597e807e36fe9866127b98dc31f5069
Luciakestel <luciakestelpires@gmail.com>
2023-01-18 20:43:12 +0100

    Excersice 2, create an array with my data

---
+++ ejercicio-2/index.js
@@ -0,0 +1,18 @@
+// Crea un nuevo archivo JS que contenga una lista con los siguientes elementos:
+// - Tu nombre (string)
+// - Tu edad (number)
+// - ¿Eres desarrollador? (boolean)
+// - Tu fecha de nacimiento (Date)
+// - Tu libro favorito (Objeto con propiedades: titulo, autor, fecha, url)
+
+const luciaData = [
+  'Lucía',
+  27,
+  true,
+  new Date(1995, 4, 31),
+  {
+    title: 'Harry Potter',
+    author: 'J K Rowling',
+    date: '1997-2007',
+  },
+];
